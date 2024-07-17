747b01d0d05a522a84d65be6f9d35a433fc89546
jista <jista@Jista_George>
2024-07-17 07:58:27 +0530

    Verify Login Page

---
+++ Automation_Project_7rMart_Supermarket/src/test/java/MyProject_TestScripts/Base.java
@@ -0,0 +1,27 @@
+package MyProject_TestScripts;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class Base {
+public WebDriver driver;    
+	
+	@BeforeMethod
+	public void initializeBrowser()
+	{
+	driver=new ChromeDriver(); 
+	driver.get("https://groceryapp.uniqassosiates.com/admin/login"); 
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	driver.manage().window().maximize(); 
+	}	
+	@AfterMethod
+	
+	public void driverQuit()   
+	{
+	driver.quit();	
+	}
+}
