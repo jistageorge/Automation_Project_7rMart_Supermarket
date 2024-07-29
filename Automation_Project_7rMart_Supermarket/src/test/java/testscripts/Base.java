9ec4cc09da57d21c36ce5b19200e6a3630146830
Jista <jistathaliyangeorge@gmail.com>
2024-07-29 09:54:20 +0530

    Until Extent Report Generation

--- Automation_Project_7rMart_Supermarket/src/test/java/testscripts/Base.java
+++ Automation_Project_7rMart_Supermarket/src/test/java/testscripts/Base.java
@@ -4,23 +4,38 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 public class Base {
 public WebDriver driver;    
 	
-	@BeforeMethod
-	public void initializeBrowser()
+	@BeforeMethod(alwaysRun=true)
+	@Parameters("browser")
+	public void initializeBrowser(String browser) throws Exception
 	{
-	driver=new ChromeDriver(); 
+		if(browser.equalsIgnoreCase("chrome")) {
+			driver=new ChromeDriver(); 	
+		}
+		else if(browser.equalsIgnoreCase("edge")){
+			driver=new EdgeDriver(); 	
+		}
+		else if(browser.equalsIgnoreCase("firefox")){
+			driver=new FirefoxDriver(); 	
+		}
+		else {
+			throw new Exception("Browser is not connected");
+		}
+	
 	driver.get("https://groceryapp.uniqassosiates.com/admin/login"); 
 	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	driver.manage().window().maximize(); 
 	System.out.println();
 	}	
-	@AfterMethod
-	
+	@AfterMethod(alwaysRun=true)	
 	public void driverQuit()   
 	{
 	driver.quit();	
